a868f3e0717d68c0d87c0e97048a2e9070d7620c
jakaria242 <jakaria.dev242@gmail.com>
2024-01-05 20:08:56 +0600

    project setup

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import TextField from '@mui/material/TextField';
+
+const Login = () => {
+  return (
+    <>
+     <TextField id="standard-basic" label="Email Address" variant="standard" />
+    </>
+  )
+}
+
+export default Login
