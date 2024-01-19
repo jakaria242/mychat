e6941f8903595750366a29af96c0588ecbe7754c
jakaria242 <jakaria.dev242@gmail.com>
2024-01-19 21:09:37 +0600

    login validation done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,6 +14,7 @@ import AuthNavigate from '../../components/AuthNavigate';
 import { FaRegEye, FaRegEyeSlash,} from "react-icons/fa6";
 import { IoCloseSharp } from "react-icons/io5";
 import Modal from '@mui/material/Modal';
+import { Alert } from '@mui/material';
 
 
 const style = {
@@ -28,10 +29,6 @@ const style = {
   p: 4,
 };
 
-
-
-
-
 const Login = () => {
   const [hideShoew, setHideShoew] = useState(false);
   
@@ -42,8 +39,67 @@ const Login = () => {
   let handleModalClose = () => {
     setOpen(false)
   }
+  
+  let [forgetError, setForgetError] = useState()
+
+  let [forgetData, setForgetData] = useState()
+  let handleForget = (e) => {
+    setForgetData(e.target.value)
+  }
+
+  let handleSend = () => {
+    if(!forgetData){
+      setForgetError("Enter your email");
+    }else {
+      if (!forgetData.match(emailregex)){
+        setForgetError("Invalid email address");
+      }else{
+        setForgetError("")
+        setForgetData("")
+      }
+    }
+    console.log(forgetData);
+  }
+
+  let [loginError, setLoginError] = useState({
+    email : "",
+    password: "",
+  })
+
+  let [signInData, setSignInData] = useState({
+    email : "",
+    password: "",
+  })
 
 
+   let handleForm = (e)=> {
+    let {name,value} = e.target
+    setSignInData({
+      ...signInData, [name]:value
+    })
+   }
+  
+   let emailregex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+   let passwordregex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+  let handleLogin = ()=> {
+    if(!signInData.email){
+      setLoginError({email: "Enter your email"});
+    }else if(!signInData.email.match(emailregex)){
+      setLoginError({email:"Invalid email address"});
+    }else if(!signInData.password){
+      setLoginError({password: "Enter your password"});
+    }else if(!signInData.password.match(passwordregex)){
+      setLoginError({password:"Use strong password"});
+    }else{
+      setLoginError({
+        email : "",
+        password: "",
+      })
+      console.log(signInData);
+    }
+  }
   return (
 <>
   <Box>
@@ -58,15 +114,23 @@ const Login = () => {
               </div>
               <div className='from_main'>
                 <div>
-                  <Input style='login_input_field' type='email' name='email' label='Email Addres' placeholder='Enter your email' variant='standard'/>
+                  <Input onChange={handleForm} style='login_input_field' type='email' name='email' label='Email Addres' placeholder='Enter your email' variant='standard'/>
+                  {
+                   loginError.email &&
+                     <Alert className='regierroe' severity="error">{loginError.email}</Alert>    
+                  }
                 </div>
-                <div>
-                  <Input style='login_input_field' type={hideShoew ? 'text' : 'password'} name='password' label='password' placeholder='Enter your password' variant='standard'/>
+                <div className='login_pass_shoeicon'>
+                  <Input onChange={handleForm} style='login_input_field' type={hideShoew ? 'text' : 'password'} name='password' label='password' placeholder='Enter your password' variant='standard'/>
                   <div className="hide_show_login"  onClick={()=>setHideShoew(!hideShoew)}>
                   {hideShoew ? <FaRegEye /> : <FaRegEyeSlash />}
                   </div>
+                  {
+                   loginError.password &&
+                     <Alert className='regierroe' severity="error">{ loginError.password}</Alert>    
+                  }
                 </div>
-                  <CustomButton styling='loginbtn' variant="contained" text='Login to Continue'/>
+                  <CustomButton onClick={handleLogin} styling='loginbtn' variant="contained" text='Login to Continue'/>
               </div>
               <AuthNavigate style='loginauth' text='Don’t have an account ?'  linktext='Sign up' link='/registration'/>
               <p className='loginauth'>
@@ -95,8 +159,12 @@ const Login = () => {
           </div>
             <div className="forgot_box">
               <h2>Forgotten password?</h2>
-              <Input  type='email' name='email' label='Email Addres' placeholder='Enter your email' variant='standard'/>
-              <CustomButton variant="contained" text='Send link'/>
+                  <Input onChange={handleForget}  type='email' name='email' label='Email Addres' placeholder='Enter your email' variant='standard'/>
+                  {
+                    forgetError && <Alert className='regierroe' severity="error">{forgetError}</Alert> 
+                  }
+              <CustomButton onClick={handleSend} variant="contained" text='Send link'/>
+
             </div>
         </Box>
     </Modal>
